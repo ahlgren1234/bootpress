cdd019173905d0d100f7cc2800e30791ff5ab4a8
Peter Ahlgren <ahlgren1234@gmail.com>
2020-02-19 15:37:14 +0100

    sass and js
    added sass and js

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function BootPress_enqueue()
 {
     wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
     if ($_SERVER['SERVER_NAME'] != 'localhost') {
-        wp_enqueue_style('style', get_template_directory_uri() . '/style.min.css');
+        wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
     } else {
         wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
     }
